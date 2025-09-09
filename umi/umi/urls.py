e693d9fd297931f83cf9516fd5acd9b66a447c8e
mvgarc <mariav2601@gmail.com>
2025-09-08 23:08:54 -0400

    Creación un urls.py dentro de gestion/ y además se agregó una vista simple

--- umi/umi/urls.py
+++ umi/umi/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-from gestion.admin.custom_site import custom_admin_site
+from django.contrib import admin
 
 urlpatterns = [
-    path("admin/", custom_admin_site.urls),
-    path("", include("gestion.urls")),
+    path("admin/", admin.site.urls),
 ]
