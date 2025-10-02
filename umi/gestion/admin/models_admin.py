b2987b76c964f9e792db069a3cf2d31e11b0c95d
mvgarc <mariav2601@gmail.com>
2025-10-02 11:04:55 -0400

    importar los modelos desde gestion.models (la app principal), no desde el submódulo admin

--- umi/gestion/admin/models_admin.py
+++ umi/gestion/admin/models_admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
-from .models import ShippingLine, Container, Document, PaymentPlan
+from gestion.models import ShippingLine, Container, Document, PaymentPlan
 from .resources import (
     ContainerResource, DocumentResource,
     PaymentPlanResource, ShippingLineResource
